c02a01f27d9254cd1303bcd1b40681649c298075
elham-ya <elham.yadegary@gmail.com>
2024-08-20 08:52:00 +0330

    working on useJWT

--- src/Hooks/useJWT.js
+++ src/Hooks/useJWT.js
@@ -1,14 +1,16 @@
-import React from "react";
 import { useNavigate } from "react-router-dom";
 
 function useJWT() {
   const navigate = useNavigate();
+
   const refreshToken = () => {
-    let refresh = localStorage.getItem("refresh");
+    const url = "http://127.0.0.1:4000/api/token";
+    const refresh = localStorage.getItem("refresh");
     const axios = require("axios");
+
     return new Promise((resolve, reject) => {
       axios
-        .post("http://127.0.0.1:4000/api/token", {
+        .post(url, {
           token: refresh,
         })
         .then((response) => {
@@ -17,17 +19,21 @@ function useJWT() {
             "refresh",
             JSON.stringify(response.data.refresh)
           );
+          resolve(response);
         })
-        .catch(function (error) {
+        .catch((error) => {
           console.log("error:", error);
+          reject(error);
         });
     });
   };
 
   function login(email, password) {
+    const url = "http://127.0.0.1:4000/api/login";
     const axios = require("axios");
+
     return new Promise((resolve, reject) => {
-      axios.post("http://127.0.0.1:4000/api/login", { email, password }).then(
+      axios.post(url, { email, password }).then(
         (response) => {
           localStorage.setItem("access", JSON.stringify(response.data.access));
           localStorage.setItem(
@@ -35,41 +41,58 @@ function useJWT() {
             JSON.stringify(response.data.refresh)
           );
           navigate("/");
-          resolve();
+          resolve(response);
         },
         function (error) {
-          console.log("error:", error);
-          reject();
+          reject(error);
         }
       );
     });
   }
 
+  // sendPostRequest NOT done
   const sendPostRequest = (url, data) => {
-    let access = localStorage.getItem("access");
-    const sendRequest = fetch(url, {
-      method: "POST",
-      body: data,
-      headers: {
-        "Content-Type": "application/json; charset=UTF-8",
-        jwt: access,
-      },
+    const axios = require("axios");
+    const access = localStorage.getItem("access");
+
+    return new Promise((resolve, reject) => {
+      axios
+        .post(url, data, {
+          headers: {
+            "Content-Type": "application/json; charset=UTF-8",
+            jwt: access,
+          },
+        })
+        .then((response) => {
+          resolve(response);
+        })
+        .catch((error) => {
+          // here we should send another request just one time
+        });
     });
   };
 
-  let refresh = localStorage.getItem("refresh");
-
   const logout = () => {
+    const url = "http://127.0.0.1:4000/api/logout";
     const axios = require("axios");
-    axios
-      .delete("http://127.0.0.1:4000/api/logout", {
-        refresh,
-      })
-      .then(() => {
-        navigate("/login");
-      });
+    const refresh = localStorage.getItem("refresh");
 
-    localStorage.clear();
+    return new Promise((resolve, reject) => {
+      axios
+        .delete(url, {
+          refresh,
+        })
+        .then(() => {
+          navigate("/login");
+          resolve();
+        })
+        .catch((error) => {
+          reject(error);
+        })
+        .finally(() => {
+          localStorage.clear();
+        });
+    });
   };
 
   return {
